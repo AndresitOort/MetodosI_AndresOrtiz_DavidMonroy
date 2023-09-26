5eef0fe1d6e5a468064d55ac9a9ace9bfe4667aa
DavidFMH1 <david_monroyhernandez@hotmail.com>
2023-09-26 18:15:17 -0500

    ejercicios parcial

--- parcial_2/Ejercicios_parcial2(5,10,18,19).py
+++ parcial_2/Ejercicios_parcial2(5,10,18,19).py
@@ -7,48 +7,6 @@ import math
 
 _x = sym.symbols('x')
 
-def GetNewton(f,df,xn,itmax=10000,precision=1e-12):
-    
-    error = 1.
-    it = 0
-    
-    while error >= precision and it < itmax:
-        
-        try:
-            
-            xn1 = xn - f(xn)/df(xn)
-            
-            error = np.abs(f(xn)/df(xn))
-            
-        except ZeroDivisionError:
-            print('Zero Division')
-            
-        xn = xn1
-        it += 1
-        
-    if it == itmax:
-        return False
-    else:
-        return xn
-    
-def GetRoots(f,df,x,tolerancia = 5):
-    
-    Roots = np.array([])
-    
-    for i in x:
-        
-        root = GetNewton(f,df,i)
-
-        if  type(root)!=bool:
-            croot = np.round( root, tolerancia )
-            
-            if croot not in Roots:
-                Roots = np.append(Roots, croot)
-                
-    Roots.sort()
-    
-    return Roots
-
 def GetHermite(n,x):
     if n == 0:
         poly = sym.Number(1)
@@ -58,56 +16,23 @@ def GetHermite(n,x):
         poly = 2*x*GetHermite(n-1,x) - sym.diff(GetHermite(n-1,x),x,1)
     return sym.expand(poly,x)
 
-def DiffHermite(n,x):
-    pn = GetHermite(n,x)
-    return sym.diff(pn,x,1)
-
-def GetAllRootsGHer(n):
+def GetHermiteWR(n):
     
-    c = np.sqrt(4*n+1)
-    xn = np.linspace(-c,c,100)
+    R,W = np.polynomial.hermite.hermgauss(n)
     
-    Hermite = np.array([])
-    DHermite = np.array([])
-    
-    for i in range(n+1):
-        Hermite =  np.append(Hermite,GetHermite(i,_x))
-        DHermite = np.append(DHermite,DiffHermite(i,_x))
-    
-    poly = sym.lambdify([_x],Hermite[n],'numpy')
-    Dpoly = sym.lambdify([_x],DHermite[n],'numpy')
-    Roots = GetRoots(poly,Dpoly,xn)
-
-    if len(Roots) != n:
-        ValueError('El número de raíces debe ser igual al n del polinomio.')
-    
-    return Roots
-
-def GetWeigthsHer(n,x):
-    
-    roots = GetAllRootsGHer(n)
-    l_h1 = sym.lambdify([x],GetHermite(n-1,x),'numpy')
-    weigths = np.array([])
-    
-    for i in roots:
-        
-        w = (2**(n-1)*math.factorial(n)*np.sqrt(np.pi))/((n**2)*(l_h1(i)**2))
-            
-        weigths = np.append(weigths,w)
-    
-    return weigths
+    return (R,W)
 
 def NpolinomiosHermite(n):
     
-    for i in range(n+1):
-        if n == 0:
-            print('El polinomio 0 no tiene raices')
-        else:
-            print('polinomion n° '+str(i))
-            print(GetHermite(i,_x))
-            print(GetAllRootsGHer(i))
-            print(GetWeigthsHer(i,_x))
-
+    for i in range(1,n+1):
+        
+        wr = GetHermiteWR(i)
+        
+        print('Polinomio n° ' + str(i))
+        print(GetHermite(i,_x))
+        print(wr[0])
+        print(wr[1])
+        
 nh = 20
 
 #NpolinomiosHermite(nh)
@@ -121,8 +46,7 @@ def IntegralHermite(f,n):
     
     res = 0
     
-    r = GetAllRootsGHer(n)
-    w = GetWeigthsHer(n,_x)
+    r,w = GetHermiteWR(n)
     
     for i in range(len(r)):
         
@@ -130,7 +54,7 @@ def IntegralHermite(f,n):
     
     return res*(2/np.sqrt(np.pi))
 
-#print(IntegralHermite(function,20))
+print(IntegralHermite(function,20))
 
 #Ejercicio numero 5
 
@@ -144,59 +68,24 @@ def GetLaguerre(n,x):
    
     return sym.expand(poly,x)
 
-def DiffLaguerre(n,x):
-    pn = GetLaguerre(n,x)
-    return sym.diff(pn,x,1)
+def GetLaguerreWR(n):
+    
+    Roots,Weihts = np.polynomial.laguerre.laggauss(n)
+    
+    return (Roots,Weihts)
 
-def GetAllRootsGLag(n):
-
-    j = n+((n-1)*np.sqrt(n))
-    xn = np.linspace(0,j,round(50+((j**2)/2)))
-    
-    Legendre = np.array([])
-    DLegendre = np.array([])
-    
-    for i in range(n+1):
-        Legendre =  np.append(Legendre,GetLaguerre(i,_x))
-        DLegendre = np.append(DLegendre,DiffLaguerre(i,_x))
-    
-    poly = sym.lambdify([_x],Legendre[n],'numpy')
-    Dpoly = sym.lambdify([_x],DLegendre[n],'numpy')
-    Roots = GetRoots(poly,Dpoly,xn)
-
-    if len(Roots) != n:
-        ValueError('El número de raíces debe ser igual al n del polinomio.')
-    
-    return Roots
-
-def GetWeigthsLag(n,x):
-    
-    weigths = np.array([])
-    roots = GetAllRootsGLag(n)
-    L_n1 = sym.lambdify([x],GetLaguerre(n+1,x),'numpy')
-    
-    for i in roots:
-        
-        w = i/(((n+1)**2)*(L_n1(i))**2)
-        
-        if w not in weigths:
-            
-            weigths = np.append(weigths,w)
-            
-    return weigths
-
-def NpolinomiosLagerre(n):
+def NpolyLaguerre(n):
     
     for i in range(1,n+1):
-        if n == 0:
-            print('El polinomio 0 no tiene raices')
-        else:
-            print('polinomion n° '+str(i))
-            print(GetLaguerre(i,_x))
-            print(GetAllRootsGLag(i))
+        wr = GetLaguerreWR(i)
+        print('polinomio n° ' + str(i))
+        print(GetLaguerre(i,_x))
+        print(wr[0])
+        print(wr[1])
 
-nl = 20
-    
-print('polinomios de Laguerre')
+ng = 20
 
-NpolinomiosLagerre(nl)
+#NpolyLaguerre(ng)
+
+#Ejercicio numero 19
+
